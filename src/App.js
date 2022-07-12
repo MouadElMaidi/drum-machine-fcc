bfed642861727a518e4849c7062e6af232f1ebb1
MouadElMaidi <mouadlmaidi@gmail.com>
2022-07-12 19:37:47 +0100

    Added all functionality expect for keyboard bindings

--- src/App.js
+++ src/App.js
@@ -1,36 +1,61 @@
 import './App.css';
 import React from "react";
-/* First Bank import */
-import clap from './bank1/707-clap.wav'
-import ambientKick from './bank1/ambient_kick.wav'
-import ecKick from './bank1/ec-kik050.wav'
-import gtrchord from './bank1/gtrchord.wav'
-import openclosehh from './bank1/openclosehh.wav'
-import ordinaryclosehh from './bank1/ordinary_closed_hh.wav'
-import shamisenchord from './bank1/shamisen-chord.wav'
-import shamisenpluck from './bank1/shamisen-pluck.wav'
-import shamisentremolo from './bank1/shamisen-tremolo.wav'
+import { bank1, bank2 } from './banks';
 
 
 function App() {
-  const drumKeys = [{ key: "Q", sound: clap }, { key: "W", sound: ambientKick },
-  { key: "E", sound: ecKick }, { key: "A", sound: gtrchord }, { key: "S", sound: openclosehh },
-  { key: "D", sound: ordinaryclosehh }, { key: "Z", sound: shamisenchord },
-  { key: "X", sound: shamisenpluck }, { key: "C", sound: shamisentremolo }]
-  /* gets the file variable name
-  const varToString = varObj => Object.keys(varObj)[0]
-  const displayName = varToString({ clap })
-  */
+  const [bank, setBank] = React.useState(bank1)
+  const [soundPlayed, setSoundPlayed] = React.useState("")
+  const [power, setPower] = React.useState(true);
+  const [volume, setVolume] = React.useState("30");
 
-  function playAudio(event) {
+  function playAudio(event, soundName) { 
+    //get the corresponding audiofile
     const audioDOM = event.target.firstElementChild;
+    //Setting the volume then playing the audio
+    audioDOM.load();
+    audioDOM.volume = volume / 100;
     audioDOM.play();
+    //Sets the display to the corresponding sound
+    setSoundPlayed(soundName);
   }
 
-  const drumPads = drumKeys.map(drumpad => {
+  function togglePower() {
+    if (power) {
+      setSoundPlayed("")
+    }
+    setPower(prev => !prev);
+  }
+
+  function handleVolume(event) {
+    const volume = event.target.value;
+    setVolume(volume);
+    setSoundPlayed("Volume: " + volume)
+  }
+
+  React.useEffect(() => {
+    setTimeout(
+      () => setSoundPlayed(""), 1000
+    )
+  }, [volume])
+
+  function toggleBank() {
+    const key = bank.find((e) => e.soundName === soundPlayed).key
+    if(bank === bank1) {
+      setSoundPlayed(bank2.find((e) => e.key === key).soundName)
+      setBank(bank2)
+    } else {
+      setSoundPlayed(bank1.find((e) => e.key === key).soundName)
+      setBank(bank1)
+    }
+    //bank === bank1 ? setBank(bank2) : setBank(bank1)
+  }
+
+  const drumPads = bank.map(drumpad => {
     return (
       <button className="drum-pad" key={drumpad.key}
-        onClick={(event) => playAudio(event)}>
+        onClick={(event) => playAudio(event, drumpad.soundName)}
+        disabled={!power}>
         {drumpad.key}
         <audio>
           <source src={drumpad.sound} type="audio/wav"></source>
@@ -47,19 +72,23 @@ function App() {
       <div className='controller'>
         <div className="toggler">
           <p>Power</p>
-          <div className="toggler--slider">
+          <div className={power ? "toggler--slider on" : "toggler--slider off"}
+            onClick={togglePower}>
             <div className="toggler--slider--square"></div>
           </div>
         </div>
         <div className="display">
-          Close HH
+          {soundPlayed}
         </div>
         <div className="range">
-          <input type="range" name="vol" id="vol" min="0" max="100" className="slider" />
+          <input type="range" name="vol" id="vol" min="0" max="100" className="slider" value={volume}
+            onChange={(event) => handleVolume(event)}
+          />
         </div>
         <div className="toggler">
           <p>Bank</p>
-          <div className="toggler--slider">
+          <div className={bank === bank1 ? "toggler--slider off" : "toggler--slider on"}
+            onClick={toggleBank}>
             <div className="toggler--slider--square"></div>
           </div>
         </div>

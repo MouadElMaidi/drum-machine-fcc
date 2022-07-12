32db34fc9ac33a66b991235a276a8f978819d802
MouadElMaidi <mouadlmaidi@gmail.com>
2022-07-12 20:26:54 +0100

    Added keyboard functionality

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [power, setPower] = React.useState(true);
   const [volume, setVolume] = React.useState("30");
 
-  function playAudio(event, soundName) { 
+  function playAudio(event, soundName) {
     //get the corresponding audiofile
     const audioDOM = event.target.firstElementChild;
     //Setting the volume then playing the audio
@@ -40,22 +40,55 @@ function App() {
   }, [volume])
 
   function toggleBank() {
-    const key = bank.find((e) => e.soundName === soundPlayed).key
-    if(bank === bank1) {
-      setSoundPlayed(bank2.find((e) => e.key === key).soundName)
-      setBank(bank2)
-    } else {
-      setSoundPlayed(bank1.find((e) => e.key === key).soundName)
-      setBank(bank1)
+    if (power) {
+      const key = bank.find((e) => e.soundName === soundPlayed).key
+      if (bank === bank1) {
+        setSoundPlayed(bank2.find((e) => e.key === key).soundName)
+        setBank(bank2)
+      } else {
+        setSoundPlayed(bank1.find((e) => e.key === key).soundName)
+        setBank(bank1)
+      }
     }
-    //bank === bank1 ? setBank(bank2) : setBank(bank1)
   }
 
+  //Add keybinding functionality
+  React.useEffect(() => {
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "Q" || event.key === 'q') {
+        document.getElementById("Q").click();
+      }
+      if (event.key === "W" || event.key === 'w') {
+        document.getElementById("W").click();
+      }
+      if (event.key === "E" || event.key === 'e') {
+        document.getElementById("E").click();
+      }
+      if (event.key === "A" || event.key === 'a') {
+        document.getElementById("A").click();
+      }
+      if (event.key === "S" || event.key === 's') {
+        document.getElementById("S").click();
+      }
+      if (event.key === "D" || event.key === 'd') {
+        document.getElementById("D").click();
+      }
+      if (event.key === "Z" || event.key === 'z') {
+        document.getElementById("Z").click();
+      }
+      if (event.key === "X" || event.key === 'x') {
+        document.getElementById("X").click();
+      }
+      if (event.key === "C" || event.key === 'c') {
+        document.getElementById("C").click();
+      }
+    })
+  }, [])
   const drumPads = bank.map(drumpad => {
     return (
       <button className="drum-pad" key={drumpad.key}
         onClick={(event) => playAudio(event, drumpad.soundName)}
-        disabled={!power}>
+        disabled={!power} id={drumpad.key}>
         {drumpad.key}
         <audio>
           <source src={drumpad.sound} type="audio/wav"></source>
@@ -82,13 +115,13 @@ function App() {
         </div>
         <div className="range">
           <input type="range" name="vol" id="vol" min="0" max="100" className="slider" value={volume}
-            onChange={(event) => handleVolume(event)}
+            onChange={(event) => handleVolume(event)} disabled={!power}
           />
         </div>
         <div className="toggler">
           <p>Bank</p>
           <div className={bank === bank1 ? "toggler--slider off" : "toggler--slider on"}
-            onClick={toggleBank}>
+            onClick={toggleBank} disabled={!power}>
             <div className="toggler--slider--square"></div>
           </div>
         </div>

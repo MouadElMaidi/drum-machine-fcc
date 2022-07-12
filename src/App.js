e60a5a93225c002fc38ea145193b4de5bc80ee8b
MouadElMaidi <mouadlmaidi@gmail.com>
2022-07-12 13:23:02 +0100

    Added styles to match the FCC example

--- src/App.js
+++ src/App.js
@@ -2,7 +2,38 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
+      <div className="button-grid">
+        <button className="drum-pad">q</button>
+        <button className="drum-pad">q</button>
+        <button className="drum-pad">q</button>
+        <button className="drum-pad">q</button>
+        <button className="drum-pad">q</button>
+        <button className="drum-pad">q</button>
+        <button className="drum-pad">q</button>
+        <button className="drum-pad">q</button>
+        <button className="drum-pad">q</button>
+      </div>
+      <div className='controller'>
+        <div className="toggler">
+          <p>Power</p>
+          <div className="toggler--slider">
+            <div className="toggler--slider--square"></div>
+          </div>
+        </div>
+        <div className="display">
+          Close HH
+        </div>
+        <div className="range">
+          <input type="range" name="vol" id="vol" min="0" max="100" className="slider"/>
+        </div>
+        <div className="toggler">
+          <p>Bank</p>
+          <div className="toggler--slider">
+            <div className="toggler--slider--square"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

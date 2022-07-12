1449067aedad3f89f041dcdc709b574bc9528bbe
MouadElMaidi <mouadlmaidi@gmail.com>
2022-07-12 15:26:08 +0100

    added audio source files and associated each drum pad element with a corresponding audio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import './App.css';
+import React from "react";
+import clap from './bank1/707-clap.wav'
+import ambientKick from './bank1/ambient_kick.wav'
+import ecKick from './bank1/ec-kik050.wav'
+import gtrchord from './bank1/gtrchord.wav'
+import openclosehh from './bank1/openclosehh.wav'
+import ordinaryclosehh from './bank1/ordinary_closed_hh.wav'
+import shamisenchord from './bank1/shamisen-chord.wav'
+import shamisenpluck from './bank1/shamisen-pluck.wav'
+import shamisentremolo from './bank1/shamisen-tremolo.wav'
+
 
 function App() {
+  const drumKeys = [{ key: "Q", sound: clap }, { key: "W", sound: ambientKick },
+  { key: "E", sound: ecKick }, { key: "A", sound: gtrchord }, { key: "S", sound: openclosehh },
+  { key: "D", sound: ordinaryclosehh }, { key: "Z", sound: shamisenchord },
+  { key: "X", sound: shamisenpluck }, { key: "C", sound: shamisentremolo }]
+  /* gets the file variable name
+  const varToString = varObj => Object.keys(varObj)[0]
+  const displayName = varToString({ clap })
+  */
+
+  const drumPads = drumKeys.map(drumpad => {
+    return (
+      <div className="drum-pad" key={drumpad.key}>
+        {drumpad.key}
+        <audio controls>
+          <source src={drumpad.sound} type="audio/wav"></source>
+        </audio>
+      </div>
+    )
+  })
+
   return (
     <div className="container">
       <div className="button-grid">
-        <button className="drum-pad">q</button>
-        <button className="drum-pad">q</button>
-        <button className="drum-pad">q</button>
-        <button className="drum-pad">q</button>
-        <button className="drum-pad">q</button>
-        <button className="drum-pad">q</button>
-        <button className="drum-pad">q</button>
-        <button className="drum-pad">q</button>
-        <button className="drum-pad">q</button>
+        {drumPads}
       </div>
       <div className='controller'>
         <div className="toggler">
@@ -25,7 +48,7 @@ function App() {
           Close HH
         </div>
         <div className="range">
-          <input type="range" name="vol" id="vol" min="0" max="100" className="slider"/>
+          <input type="range" name="vol" id="vol" min="0" max="100" className="slider" />
         </div>
         <div className="toggler">
           <p>Bank</p>
@@ -39,3 +62,4 @@ function App() {
 }
 
 export default App;
+
